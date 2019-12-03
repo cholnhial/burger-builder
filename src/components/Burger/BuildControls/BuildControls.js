902ce223c99ac04e66eff95a2003dacbfac58f83
Chol Nhial <chol.nhial.chol@gmail.com>
2019-12-03 15:49:34 +1000

    Implement logic for adding ingredients to burger

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -12,7 +12,7 @@ const controls = [
 const buildControls = (props) => (
     <div className={classes.BuildControls}>
         {controls.map(ctrl => (
-            <BuildControl key={ctrl.label} label={ctrl.label}></BuildControl>
+            <BuildControl added={() => props.ingredientAdded(ctrl.type)} key={ctrl.label} label={ctrl.label}></BuildControl>
         ))}
     </div>
 );
