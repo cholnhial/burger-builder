904e0b58875221e17393bb1f2cf0e2571a0a3161
Chol Nhial <chol.nhial.chol@gmail.com>
2019-12-03 16:29:00 +1000

    Add the price

--- src/container/BurgerBuilder/BurgerBuilder.js
+++ src/container/BurgerBuilder/BurgerBuilder.js
@@ -77,6 +77,7 @@ class BurgerBuilder extends Component {
             <Aux>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls
+                    price={this.state.totalPrice}
                     disabledInfo={disabledInfo}
                     ingredientAdded={this.addIngredientHandler}
                     ingredientRemoved={this.removeIngredientHandler}
